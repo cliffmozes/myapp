c31c175f631b5e8793bf76a732a1226314f7ae42
Moses Cliff <mozescliff@gmail.com>
2018-03-03 13:26:12 +0300

    First trial

---
+++ home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Second Page</title>
+  </head>
+  <body>
+    <?php
+    case
+
+
+
+     ?>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My app is good</title>
+	</head>
+	<body>
+<?php
+
+		for ($i=0; $i < 9; $i++) {
+			# code...
+		echo("Hello World"."<br>");
+		}
+?>
+
+	</body>
+</html>\
